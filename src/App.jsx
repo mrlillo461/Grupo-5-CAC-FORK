628fb833a0f85980e06e55447650f295b2ec6915
Jose Aragon <115207491+mrlillo461@users.noreply.github.com>
2023-06-26 04:28:12 -0300

    Add ProductCard structure and ProductCard.css file
    Modify App.jsx to render ProductCard component using React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+// import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Home from "../src/pages/Home/Home";
+// import Home from "../src/pages/Home/Home";
 import ProductDetails from "../src/pages/ProductsDetails/ProductDetails";
-import Cart from "../src/components/Cart/Cart";
-import NotFound from "../src/components/NotFound/NotFound";
-
+// import Cart from "../src/components/Cart/Cart";
+// import NotFound from "../src/components/NotFound/NotFound";
+import ProductCard from "../src/components/ProductCard/ProductCard";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home/>} />
+        <Route exact path="/" element={ <ProductCard /> } />
+          {/* <Route exact path="/" element={<Home/>} /> */}
           <Route path="/product/:id" element={<ProductDetails/>} />
-          <Route path="/cart" element={<Cart/>} />
-          <Route element={<NotFound/>} />
+          {/* <Route path="/cart" element={<Cart/>} /> */}
+          {/* <Route element={<NotFound/>} /> */}
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
+import "./ProductCard.css"
+import AirMax from "./ID1_Air_Max_1.png";  
 
-function ProductCard({ product }) {
+function ProductCard() {
   return (
-    <div>
-      <h3>{product.name}</h3>
-      <p>Price: ${product.price}</p>
-      <Link to={`/product/${product.id}`}>View Details</Link>
+    <div className="container">
+      <div className="card">
+        <div className="card-img">
+          <img src={ AirMax } alt="ZAPAZ" />
+        </div>
+        <div className="card-content">
+         <h2 className="card-title">Air Max 1</h2>
+         <div className="size">
+           <h3>Size :</h3>
+           <span>42</span>
+         </div>
+         <div className="color">
+           <h3>Color :</h3>
+           <span></span>
+         </div>
+         <Link to={`/product/1`}>More Details</Link>
+       </div>
+      </div>
     </div>
   );
+
+  // function ProductCard({ product }) {
+  //   return (
+  //     <div>
+  //       <h3>{product.name}</h3>
+  //       <p>Price: ${product.price}</p>
+  //       <Link to={`/product/${product.id}`}>View Details</Link>
+  //     </div>
+  //   );
 }
 
 export default ProductCard;
